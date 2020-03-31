25f92a26fa0ab6547f42b691aad3807389148ddc
Frederik Wilde <wilde.physics@gmail.com>
2020-03-31 18:34:05 +0200

    Gates class removed. The load_classical_ham method needs to be rewritten. Many methods are not yet implemented.

--- qradient/physical_components/state.py
+++ qradient/physical_components/state.py
@@ -12,6 +12,31 @@ _proj0 = sp.coo_matrix([[1., 0.], [0., 0.]], dtype='complex')
 _proj1 = sp.coo_matrix([[0., 0.], [0., 1.]], dtype='complex')
 
 class State:
+    '''
+    Objects of this type can contain up to three attributes. A state vector
+    is always contained. Additionally a left-hand-side matrix can be stored to
+    track the reverse circuit as well as a center matrix where gates are applied
+    to both sides (for sandwhiching with a state).
+
+    For all these attributes the methods provided apply gates. Those gates must
+    be loaded prior to application.
+
+    Args:
+        qubit_number (int): The system size.
+        ini (str):
+            The initial state, which is also the reference for the reset
+            method.
+
+    Attributes:
+        vec (np.ndarray): The state vector
+        lhs (scipy.sparse.csr_matrix):
+            A left-hand-side matrix initialized to the identity. It will
+            quickly become a dense matrix, but for multiplication with sparse
+            gates it is stored as a sparse matrix.
+        center_matrix (scipy.sparse.csr_matrix):
+            A center matrix, where gates are applied on both sides
+            simultaneously.
+    '''
     def __init__(self, qubit_number, ini='0'):
         self.__qnum = qubit_number
         self.__ini = ini
@@ -129,75 +154,67 @@ class State:
         self.vec = .5*np.cos(.5*angle) * self.__yrot[i].dot(self.vec) - \
             .5*np.sin(.5*angle) * self.vec
 
+    def yrot_lhs(self, angle, i):
+        warnings.warn('Not implemented.')
+
+    def yrot_center_matrix(self, angle, i):
+        warnings.warn('Not implemented.')
+
     ############################################################################
     # z rotations
-    def zrot(self, angle, i):
-        self.vec = np.exp(-.5j*angle) * self.zrot_pos[i] * self.vec + \
-            np.exp(.5j*angle) * self.zrot_neg[i] * self.vec
-
-    def dzrot(self, angle, i):
-        '''Derivative of zrot.'''
-        self.vec = -.5j * np.exp(-.5j*angle) * self.zrot_pos[i] * self.vec + \
-            .5j * np.exp(.5j*angle) * self.zrot_neg[i] * self.vec
-
-    def cnot(self, i, j):
-        self.vec = self.cnots[i, j].dot(self.vec)
-
-    def cnot_ladder(self, stacking):
-        '''Applies a one-dimensional ladder of CNOT gates
-
-        Args:
-            stacking (int): The even to uneven qubits are coupled first (i.e. 0-1,
-                2-3, ... and 1-2, 3-4, ... in the second layer) for stacking=0.
-                For stacking=1 the order is reversed.
-        '''
-        self.vec = self.cnot_ladder[stacking].dot(self.vec)
-
-    def exp_ham_classical(self, angle):
-        '''Rotate around a classical Hamiltonian, as done in QAOA.'''
-        self.vec *= np.exp(-1.j * angle * self.classical_ham)
-
-    def exp_ham_classical_component(self, angle, i):
-        '''Rotate around a classical Hamiltonian component, as done in QAOA.'''
-        self.vec *= np.exp(-1.j * angle * self.classical_ham_components[i])
-
-    def ham_classical(self):
-        '''Multiply a classical Hamiltonian (incl. -1.j) with the state. For derivatives.'''
-        self.vec *= -1.j * self.classical_ham
-
-    def norm_error(self):
-        return 1. - np.linalg.norm(self.vec)
-
-class Gates:
-
-
-
-
-    def add_zrots(self):
-        self.zrot_pos = np.zeros([self.__qnum, 2**self.__qnum], dtype='complex')
-        self.zrot_neg = np.zeros([self.__qnum, 2**self.__qnum], dtype='complex')
+    def load_zrots(self):
+        self.__zrot_pos = np.zeros([self.__qnum, 2**self.__qnum], dtype='complex')
+        self.__zrot_neg = np.zeros([self.__qnum, 2**self.__qnum], dtype='complex')
         for i in range(self.__qnum):
             interval = 2**(self.__qnum-1-i)
             for j in range(0, 2**self.__qnum, int(2*interval)):
-                self.zrot_pos[i, j: j+interval] += 1.
-                self.zrot_neg[i, j+interval: j+2*interval] += 1.
+                self.__zrot_pos[i, j: j+interval] += 1.
+                self.__zrot_neg[i, j+interval: j+2*interval] += 1.
 
-    def add_cnots(self, which):
+    def zrot(self, angle, i):
+        self.vec = np.exp(-.5j*angle) * self.__zrot_pos[i] * self.vec + \
+            np.exp(.5j*angle) * self.__zrot_neg[i] * self.vec
+
+    def dzrot(self, angle, i):
+        '''Derivative of zrot.'''
+        self.vec = -.5j * np.exp(-.5j*angle) * self.__zrot_pos[i] * self.vec + \
+            .5j * np.exp(.5j*angle) * self.__zrot_neg[i] * self.vec
+
+    def zrot_lhs(self, angle, i):
+        warnings.warn('Not implemented.')
+
+    def zrot_center_matrix(self, angle, i):
+        warnings.warn('Not implemented.')
+
+    ############################################################################
+    # CNOTs
+    def load_cnots(self, which):
         '''Adds CNOT gates into memory.
 
         Args:
             which (array[bool]): qnum by qnum matrix which specifies which CNOTs to add.
                 Diagonal entries must be False.
         '''
-        self.cnots = np.ndarray([self.__qnum, self.__qnum], dtype=sp.csr_matrix)
+        self.__cnots = np.ndarray([self.__qnum, self.__qnum], dtype=sp.csr_matrix)
         for i in range(self.__qnum):
             for j in range(self.__qnum):
                 if which[i, j]:
-                    self.cnots[i, j] = self.__cnot(i, j)
+                    self.__cnots[i, j] = _cnot(i, j)
 
-    def add_cnot_ladder(self, periodic=False):
+    def cnot(self, i, j):
+        self.vec = self.__cnots[i, j].dot(self.vec)
+
+    def cnot_lhs(self, i, j):
+        warnings.warn('Not implemented.')
+
+    def cnot_center_matrix(self, i, j):
+        warnings.warn('Not implemented.')
+
+    ############################################################################
+    # CNOT ladder
+    def load_cnot_ladder(self, periodic=False):
         # periodic ladders with uneven qubit number is ambiguous
-        if periodic and (self.__qnum%2 != 0):
+        if periodic and (self.__qnum % 2 != 0):
             raise ValueError((
                 'CNOT gates in a ladder structure with periodic boundaries',
                 'are ambiguous for uneven number of qubits.'
@@ -213,28 +230,47 @@ class Gates:
         for i in range(self.__qnum):
             for j in range(self.__qnum):
                 if which[i, j]:
-                    cnots[i, j] = self.__cnot(i, j)
+                    cnots[i, j] = _cnot(i, j)
         # multiply them
         id = sp.identity(2**self.__qnum, dtype='complex', format='csr')
-        self.cnot_ladder = np.array([id, id])
+        self.__cnot_ladder = np.array([id.copy(), id.copy()])
         if periodic:
             upper = self.__qnum
         else:
             upper = self.__qnum - 1
         for i in range(0, upper, 2):
-            self.cnot_ladder[0] = self.cnot_ladder[0].dot(cnots[i, (i+1)%self.__qnum])
+            self.__cnot_ladder[0] = self.__cnot_ladder[0].dot(cnots[i, (i+1) % self.__qnum])
         for i in range(1, upper, 2):
-            self.cnot_ladder[0] = self.cnot_ladder[0].dot(cnots[i, (i+1)%self.__qnum])
-            self.cnot_ladder[1] = self.cnot_ladder[1].dot(cnots[i, (i+1)%self.__qnum])
+            self.__cnot_ladder[0] = self.__cnot_ladder[0].dot(cnots[i, (i+1) % self.__qnum])
+            self.__cnot_ladder[1] = self.__cnot_ladder[1].dot(cnots[i, (i+1) % self.__qnum])
         for i in range(0, upper, 2):
-            self.cnot_ladder[1] = self.cnot_ladder[1].dot(cnots[i, (i+1)%self.__qnum])
+            self.__cnot_ladder[1] = self.__cnot_ladder[1].dot(cnots[i, (i+1) % self.__qnum])
 
-    def add_classical_ham(self, observable, include_individual_components=False):
+    def cnot_ladder(self, stacking):
+        '''Applies a one-dimensional ladder of CNOT gates
+
+        Args:
+            stacking (int): The even to uneven qubits are coupled first (i.e. 0-1,
+                2-3, ... and 1-2, 3-4, ... in the second layer) for stacking=0.
+                For stacking=1 the order is reversed.
+        '''
+        self.vec = self.cnot_ladder[stacking].dot(self.vec)
+
+    def cnot_ladder_lhs(self, stacking):
+        warnings.warn('Not implemented.')
+
+    def cnot_ladder_center_matrix(self, stacking):
+        warnings.warn('Not implemented.')
+
+    ############################################################################
+    # classical Hamiltonian
+    def load_classical_ham(self, observable, include_individual_components=False):
         '''Creates a the full vector representing an observable, that is purely classical.
 
         Args:
             observable (Observable): An observable object.
         '''
+        #### NEEDS TO BE REWRITTEN!!!
         observable.check_observable(
             known_keys=['z', 'zz'],
             warning='Non-classical observable component found. Only \'z\' and \'zz\' are accepted in this method.'
@@ -266,24 +302,59 @@ class Gates:
             self.classical_ham_components = np.array(component_list)
         return self
 
-    def __cnot(self, i, j):
-        '''Controlled NOT gate. First argument is the control qubit.'''
-        if i < j and j < self.__qnum:
-            out1 = _skr(_id(i), _proj0)
-            out1 = _skr(out1, _id(self.__qnum-i-1))
+    def rot_classical_ham(self, angle):
+        '''Rotate around a classical Hamiltonian, as done in QAOA.'''
+        self.vec *= np.exp(-1.j * angle * self.classical_ham)
 
-            out2 = _skr(_id(i), _proj1)
-            out2 = _skr(out2, _id(j-i-1))
-            out2 = _skr(out2, _x)
-            out2 = _skr(out2, _id(self.__qnum-j-1))
-        elif i > j and i < self.__qnum:
-            out1 = _skr(_id(i), _proj0)
-            out1 = _skr(out1, _id(self.__qnum-i-1))
+    def rot_classical_ham_lhs(self, angle):
+        warnings.warn('Not implemented.')
 
-            out2 = _skr(_id(j), _x)
-            out2 = _skr(out2, _id(i-j-1))
-            out2 = _skr(out2, _proj1)
-            out2 = _skr(out2, _id(self.__qnum-i-1))
-        else:
-            raise ValueError('Invalid CNOT indecies {} and {}, for {} qubits.'.format(i, j, self.__qnum))
-        return (out1 + out2).asformat('csr')
+    def rot_classical_ham_center_matrix(self, angle):
+        warnings.warn('Not implemented.')
+
+    def rot_classical_ham_component(self, angle, i):
+        '''Rotate around a classical Hamiltonian component, as done in QAOA.'''
+        self.vec *= np.exp(-1.j * angle * self.classical_ham_components[i])
+
+    def rot_classical_ham_component_lhs(self, angle, i):
+        warnings.warn('Not implemented.')
+
+    def rot_classical_ham_component_center_matrix(self, angle, i):
+        warnings.warn('Not implemented.')
+
+    def classical_ham(self):
+        '''Multiply a classical Hamiltonian (incl. -1.j) with the state. For derivatives.'''
+        self.vec *= -1.j * self.classical_ham
+
+    def classical_ham_lhs(self):
+        warnings.warn('Not implemented.')
+
+    def classical_ham_center_matrix(self):
+        warnings.warn('Not implemented.')
+        
+    ############################################################################
+    # checking that the 2-norm is 1.
+    def norm_error(self):
+        return 1. - np.linalg.norm(self.vec)
+
+def _cnot(self, i, j):
+    '''Controlled NOT gate. First argument is the control qubit.'''
+    if i < j and j < self.__qnum:
+        out1 = _skr(_id(i), _proj0)
+        out1 = _skr(out1, _id(self.__qnum-i-1))
+
+        out2 = _skr(_id(i), _proj1)
+        out2 = _skr(out2, _id(j-i-1))
+        out2 = _skr(out2, _x)
+        out2 = _skr(out2, _id(self.__qnum-j-1))
+    elif i > j and i < self.__qnum:
+        out1 = _skr(_id(i), _proj0)
+        out1 = _skr(out1, _id(self.__qnum-i-1))
+
+        out2 = _skr(_id(j), _x)
+        out2 = _skr(out2, _id(i-j-1))
+        out2 = _skr(out2, _proj1)
+        out2 = _skr(out2, _id(self.__qnum-i-1))
+    else:
+        raise ValueError('Invalid CNOT indecies {} and {}, for {} qubits.'.format(i, j, self.__qnum))
+    return (out1 + out2).asformat('csr')
