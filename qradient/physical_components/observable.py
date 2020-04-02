2ee17773153f02441c877591637fa9a3142ccaaa
Frederik Wilde <wilde.physics@gmail.com>
2020-04-02 14:40:25 +0200

    observable is now much leaner. has one private load_matrix method and one public load_components method which does nothing if components already exist. projectors are still to do.

--- qradient/physical_components/observable.py
+++ qradient/physical_components/observable.py
@@ -15,6 +15,14 @@ class Observable:
                 'y': 1d-array for Pauli Y.
                 'z': 1d-array for Pauli Z.
                 'zz': a 2d-array as upper triangular matrix for two-qubit ZZ terms.
+
+        Attributes:
+            dict (dict): The original observable dictionary.
+            matrix (scipy.sparse.csr_matrix): The full matrix (in sparse format).
+            component_array (np.ndarray[scipy.sparse.csr_matrix]):
+                Full matrices corresponding to the individual components.
+            component_weights (np.ndarray[np.float64]):
+                The weights for each component in component_array.
         '''
         self.__qnum = next(iter(observable.values())).shape[0]
         for v in observable.values():
@@ -27,57 +35,71 @@ class Observable:
 
         # load observable
         self.dict = observable
-        self.__check_observable(known_keys=['x', 'y', 'z', 'zz'])
-        self.load_matrix(observable)
+        self.check_observable(known_keys=['x', 'y', 'z', 'zz'])
+        self.__load_matrix()
 
-
-    def load_matrix(self, observable, store_components=False):
-        self.matrix = sp.coo_matrix((2**self.qnum, 2**self.qnum), dtype='complex').asformat('csr')
-        x = sp.csr_matrix([[0., 1.], [1., 0.]], dtype='complex')
-        y = sp.csr_matrix([[0., -1.j], [1.j, 0.]], dtype='complex')
-        z = sp.csr_matrix([[1., 0.], [0., -1.]], dtype='complex')
-
-        if self.store_components:
-            component_list = []
-            component_weights = []
-
+    def __load_matrix(self):
+        self.matrix = sp.coo_matrix((2**self.__qnum, 2**self.__qnum), dtype='complex').asformat('csr')
         # single-qubit components
-        for identifier, matrix in [('x', x), ('y', y), ('z', z)]:
-            if identifier in observable:
-                for i, weight in enumerate(observable[identifier]):
+        for identifier, matrix in [('x', _x), ('y', _y), ('z', _z)]:
+            if identifier in self.dict:
+                for i, weight in enumerate(self.dict[identifier]):
                     if weight != None:
-                        Observable.__weight_check(weight, identifier)
-                        op = kr(kr(sp.identity(2**i), matrix), sp.identity(2**(self.qnum-i-1)))
+                        _weight_check(weight, identifier)
+                        op = kr(kr(_id(2**i), matrix), _id(2**(self.__qnum-i-1)))
                         self.matrix += weight * op
-                        if self.store_components:
-                            component_list.append(weight * op)
-                            component_weights.append(weight)
         # two-qubit components
-        if 'zz' in observable:
-            for i in range(self.qnum):
+        if 'zz' in self.dict:
+            for i in range(self.__qnum):
                 for j in range(i+1):
-                    if observable['zz'][i, j] != None:
+                    if self.dict['zz'][i, j] != None:
                         raise ValueError((
-                            'zz of observable should be a upper triangular {} '.format(self.qnum),
-                            'by {} matrix. Diagonal and lower triangle should'.format(self.qnum),
-                            'contain None\'s, not {}.'.format(observable['zz'][i, j])
+                            'zz of observable should be a upper triangular {} '.format(self.__qnum),
+                            'by {} matrix. Diagonal and lower triangle should'.format(self.__qnum),
+                            'contain None\'s, not {}.'.format(self.dict['zz'][i, j])
                         ))
-                for j in range(i+1, self.qnum):
-                    if observable['zz'][i, j] != None:
-                        Observable.__weight_check(observable['zz'][i, j], 'y')
-                        op = kr(kr(sp.identity(2**i), z), sp.identity(2**(j-i-1)))
-                        op = kr(kr(op, z), sp.identity(2**(self.qnum-j-1)))
-                        self.matrix += observable['zz'][i, j] * op
-                        if self.store_components:
-                            component_list.append(observable['zz'][i, j] * op)
-                            component_weights.append(observable['zz'][i, j])
-        # store the components and weights as arrays
-        if self.store_components:
-            self.num_components = len(component_weights)
-            self.component_array = np.array(component_list)
-            weight_normalization = np.sum(component_weights)
-            self.weight_distribution = np.array(component_weights)/weight_normalization
+                for j in range(i+1, self.__qnum):
+                    if self.dict['zz'][i, j] != None:
+                        _weight_check(self.dict['zz'][i, j], 'y')
+                        op = kr(kr(sp.identity(2**i), _z), _id(2**(j-i-1)))
+                        op = kr(kr(op, _z), _id(2**(self.__qnum-j-1)))
+                        self.matrix += self.dict['zz'][i, j] * op
 
+    def load_components(self):
+        if hasattr(self, 'component_array'):
+            return None
+        component_list = []
+        component_weights = []
+        # single-qubit components
+        for identifier, matrix in [('x', _x), ('y', _y), ('z', _z)]:
+            if identifier in self.dict:
+                for i, weight in enumerate(self.dict[identifier]):
+                    if weight != None:
+                        _weight_check(weight, identifier)
+                        op = kr(kr(_id(2**i), matrix), _id(2**(self.__qnum-i-1)))
+                        component_list.append(op)
+                        component_weights.append(weight)
+        # two-qubit components
+        if 'zz' in self.dict:
+            for i in range(self.__qnum):
+                for j in range(i+1):
+                    if self.dict['zz'][i, j] != None:
+                        raise ValueError((
+                            'zz of observable should be a upper triangular {} '.format(self.__qnum),
+                            'by {} matrix. Diagonal and lower triangle should'.format(self.__qnum),
+                            'contain None\'s, not {}.'.format(self.dict['zz'][i, j])
+                        ))
+                for j in range(i+1, self.__qnum):
+                    if self.dict['zz'][i, j] != None:
+                        _weight_check(self.dict['zz'][i, j], 'y')
+                        op = kr(kr(sp.identity(2**i), _z), _id(2**(j-i-1)))
+                        op = kr(kr(op, _z), _id(2**(self.__qnum-j-1)))
+                        component_list.append(op)
+                        component_weights.append(self.dict['zz'][i, j])
+        self.component_array = np.array(component_list)
+        self.weight_array = np.array(component_weights)
+        weight_normalization = np.sum(component_weights)
+        self.weight_distribution = np.array(component_weights)/weight_normalization
 
     def load_projectors(self):
         if self.has_loaded_projectors: # in case the caller has not checked
@@ -86,16 +108,16 @@ class Observable:
         # construct pauli projectors
         projectors = []
         projector_weights = []
-        Projector.set_qnum(self.qnum)
+        Projector.set_qnum(self.__qnum)
         for identifier in ['x', 'y', 'z']:
             if identifier in self.info:
-                for i in range(self.qnum):
+                for i in range(self.__qnum):
                     if self.info[identifier][i] != None:
                         projectors.append(Projector(identifier, i))
                         projector_weights.append(self.info[identifier][i])
         if 'zz' in self.info:
-            for i in range(self.qnum):
-                for j in range(self.qnum):
+            for i in range(self.__qnum):
+                for j in range(self.__qnum):
                     if self.info['zz'][i, j] != None:
                         projectors.append(Projector('zz', i, j))
                         projector_weights.append(self.info['zz'][i, j])
@@ -103,7 +125,7 @@ class Observable:
         self.projector_weights = np.array(projector_weights)
         self.has_loaded_projectors = True
 
-    def __check_observable(self, known_keys, warning=None):
+    def check_observable(self, known_keys, warning=None):
         '''Tests whether observable only contains known keys and throws a warning otherwise.'''
         for key in list(self.info.keys()):
             unknown = True
@@ -115,13 +137,6 @@ class Observable:
                     warnings.warn('Unknown element of observable {} will be ignored.'.format(key))
                 else:
                     warnings.warn(warning)
-
-    def __weight_check(weight, observable_component):
-        if abs(weight) < 10.**-15:
-            warnings.warn((
-                'Weight in observable {} is zero or almost zero.'.format(observable_component),
-                ' If you dont\'t want to include it, set it to None.'
-            ))
 
 class Projector:
     def __init__(self, key, *args):
@@ -175,3 +190,22 @@ class Projector:
             return self.array * vec
         else:
             return self.array.dot(vec)
+
+def _weight_check(weight, observable_component):
+    '''
+    Checks that component weights are not numerical zeros.
+
+    Args:
+        weight (float): The weight to check
+        observable_component (str): The name of the component.
+    '''
+    if abs(weight) < 10.**-15:
+        warnings.warn((
+            'Weight in observable {} is zero or almost zero.'.format(observable_component),
+            ' If you dont\'t want to include it, set it to None.'
+        ))
+
+_id = lambda i: sp.identity(2**i, dtype='complex', format='coo')
+_x = sp.csr_matrix([[0., 1.], [1., 0.]], dtype='complex')
+_y = sp.csr_matrix([[0., -1.j], [1.j, 0.]], dtype='complex')
+_z = sp.csr_matrix([[1., 0.], [0., -1.]], dtype='complex')
